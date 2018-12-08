49cef6588133f892c598b2b7a590248c0191a905
emya <e.ayada810004@gmail.com>
2018-12-08 15:19:44 -0800

    new frontend

---
+++ takako/takako/assets/bundles/precache-manifest.15f5f0a15efaa1ce25ea273de51ee99f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b2dbf55226a0a2ce4d51",
+    "url": "/static/bundles/static/css/main.8888a46f.chunk.css"
+  },
+  {
+    "revision": "b2dbf55226a0a2ce4d51",
+    "url": "/static/bundles/js/main.b2dbf552.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/static/bundles/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "cd71c25a96c5596961f5",
+    "url": "/static/bundles/js/runtime~main.cd71c25a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/bundles/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "860942991f99a1c5cabf8c6ca24f290d",
+    "url": "/static/bundles/index.html"
+  }
+];
--- takako/takako/settings.py
+++ takako/takako/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'webpack_loader',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'takako.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates"), ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -68,6 +69,13 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'takako.wsgi.application'
+
+WEBPACK_LOADER = {
+    'DEFAULT': {
+            'BUNDLE_DIR_NAME': 'bundles/',
+            'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats.dev.json'),
+        }
+}
 
 
 # Database
