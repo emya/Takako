41385ab32dea08ced6cc1062a4b66823ec5efd2f
emya <e.ayada810004@gmail.com>
2019-08-15 00:17:04 -0700

    remove function we dont use

--- takako/notes/permissions.py
+++ takako/notes/permissions.py
@@ -3,12 +3,8 @@ from rest_framework import permissions
 
 class BaseUserPermissions(permissions.BasePermission):
     """
-    Handles permissions for users.  The basic rules are
-
-     - owner may GET, PUT, POST, DELETE
-     - nobody else can access
-     """
-
+    Base permission class
+    """
     def has_object_permission(self, request, view, obj):
         if request.method == 'GET':
             return True
@@ -18,14 +14,8 @@ class BaseUserPermissions(permissions.BasePermission):
 
 class BaseTransactionPermissions(permissions.BasePermission):
     """
-    Handles permissions for users.  The basic rules are
-
-     - owner may GET, PUT, POST, DELETE
-     - nobody else can access
-     """
-    def has_permission(self, request, view):
-        return request.user
-
+    Rermissions for Transaction objects
+    """
     def has_object_permission(self, request, view, obj):
         # check if user is owner
         return request.user == obj.requester or request.user == obj.respondent
