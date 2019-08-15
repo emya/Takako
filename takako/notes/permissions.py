14c4743c80506050db59cb7487a97a7542cefeb6
emya <e.ayada810004@gmail.com>
2019-08-15 00:20:59 -0700

    add permission (#79)
    * add permission
    
    * remove function we dont use
    
    * add comments

---
+++ takako/notes/permissions.py
@@ -0,0 +1,21 @@
+from rest_framework import permissions
+
+
+class BaseUserPermissions(permissions.BasePermission):
+    """
+    Base permission class
+    """
+    def has_object_permission(self, request, view, obj):
+        if request.method == 'GET':
+            return True
+
+        # check if user is a given obj's user
+        return request.user == obj.user
+
+class BaseTransactionPermissions(permissions.BasePermission):
+    """
+    Rermissions for Transaction objects
+    """
+    def has_object_permission(self, request, view, obj):
+        # check if user is a given obj's requester or respondent
+        return request.user == obj.requester or request.user == obj.respondent
