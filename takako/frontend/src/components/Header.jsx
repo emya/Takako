b02516100d202a51ca1685048cb578c31471fe55
chibee1 <cikuyama@gmail.com>
2019-12-28 15:09:39 -0500

    fixed header for mobile view

--- takako/frontend/src/components/Header.jsx
+++ takako/frontend/src/components/Header.jsx
@@ -25,9 +25,11 @@ render() {
       <div class="mobile-topbar-contents">
 
         <a href="#"><img class="logo-mobile" src={require('../img/Torimo_new.png')} /></a>
+        <a href="/register" class="mobile-signin" onClick="ga('send', 'event', 'link', 'click', 'register3')">
+          <p class="mobile-header-link">Register</p>
+        </a>
         <a href="/login" class="mobile-signin" onClick="ga('send', 'event', 'link', 'click', 'signin')">
-          <img class="mobile-menu-icon" src={require('../img/signin.png')} />
-          <p class="sign-in-mobile">Sign in</p>
+          <p class="mobile-header-link">Sign in</p>
         </a>
 
       </div>
